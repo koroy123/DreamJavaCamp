dc4b379177c75a1039234ded719920fd7b1a13d2
학생50 <50@user-PC>
2019-05-05 14:15:54 +0900

    Chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,9 +1,17 @@
 package icehs.science.chapter02;
 
+/*
+ * My First Java Class
+ * 
+ */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); //나의 첫 작품
+		/*
+		 * 
+		 */
 		System.out.println("안녕하세요");
 		
 	}
